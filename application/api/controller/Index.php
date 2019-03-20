40df400e842677277144b59807d80906447cd1ef
weichengzhong <weichengzhong@hk01.com>
2019-03-20 14:52:30 +0800

    init

---
+++ application/api/controller/Index.php
@@ -0,0 +1,20 @@
+<?php
+namespace  app\api\controller;
+/**
+ * Created by PhpStorm.
+ * User: weichengzhong
+ * Date: 19-3-8
+ * Time: 下午12:17
+ */
+class Index
+{
+    public function index()
+    {
+        echo 'hello index index';
+    }
+
+    public function hello_world()
+    {
+        echo 'hello world';
+    }
+}
---
+++ application/api/controller/UserName.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\api\controller;
+/**
+ * Created by PhpStorm.
+ * User: weichengzhong
+ * Date: 19-3-8
+ * Time: 下午2:09
+ */
+class UserName
+{
+    public function index()
+    {
+        echo 'hello UserName index';
+    }
+}
